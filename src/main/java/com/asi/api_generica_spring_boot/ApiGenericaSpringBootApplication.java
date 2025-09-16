ab4b087b92777d70070c8ca5412b40b604fb6a7c
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-16 10:24:58 -0300

    first commit

---
+++ src/main/java/com/asi/api_generica_spring_boot/ApiGenericaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.asi.api_generica_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGenericaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGenericaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asi/api_generica_spring_boot/ApiGenericaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asi.api_generica_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGenericaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
