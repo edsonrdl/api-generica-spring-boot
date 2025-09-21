b7a863afe2dd2724e2fb3b038765512cf86d5e03
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-21 19:51:46 -0300

    feat: userservice

--- src/main/java/com/asi/api_generica_spring_boot/services/userservice/UserService.java
+++ src/main/java/com/asi/api_generica_spring_boot/services/userservice/UserService.java
@@ -1,4 +1,35 @@
 package com.asi.api_generica_spring_boot.services.userservice;
 
+import com.asi.api_generica_spring_boot.domain.entities.user.UserEntity;
+import com.asi.api_generica_spring_boot.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    private  final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public UserEntity createUser(UserEntity userEntity) {
+        return userRepository.save(userEntity);
+    }
+
+    public UserEntity buscarUser(Long id) {
+        UserEntity user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("User not found"));
+        return user;
+    }
+
+    public UserEntity updateUser(Long id, UserEntity user) {
+        UserEntity userData=buscarUser(id);
+
+        userData.setFullName(user.getFullName());
+
+        return userRepository.save(userData);
+    }
 }
