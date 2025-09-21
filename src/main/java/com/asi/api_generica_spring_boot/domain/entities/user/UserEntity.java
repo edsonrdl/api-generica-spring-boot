5c1b7959a636b22f59131c320c0f9f83c544f98b
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-21 19:51:01 -0300

    feat: entidade user

--- src/main/java/com/asi/api_generica_spring_boot/domain/entities/user/UserEntity.java
+++ src/main/java/com/asi/api_generica_spring_boot/domain/entities/user/UserEntity.java
@@ -1,5 +1,6 @@
-package com.asi.api_generica_spring_boot.domain.entities.task;
+package com.asi.api_generica_spring_boot.domain.entities.user;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,12 +10,24 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "\"TAB_USER\"")
 public class UserEntity {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="\"ID_USER\"")
+    private Long idUser;
+
+    @Column(name="\"FULL_NAME\"")
     private String fullName;
 
+    @Column(name="\"USER_NAME\"")
     private String userName;
 
+    @Column(name="\"PASSWORD\"")
     private String password;
 
 }
