1a6b2b71880e98352efe0e978c3d7ac91b13a5f4
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-21 19:51:32 -0300

    feat: user repositorie

--- src/main/java/com/asi/api_generica_spring_boot/repositories/UserRepository.java
+++ src/main/java/com/asi/api_generica_spring_boot/repositories/UserRepository.java
@@ -1,4 +1,10 @@
 package com.asi.api_generica_spring_boot.repositories;
+import com.asi.api_generica_spring_boot.domain.entities.user.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class UserRepository {
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
 }
