82ea954fbd16a3434f8e1c2c6f7d7da01e74a60e
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2025-09-21 19:50:54 -0300

    feat: entidade task

--- src/main/java/com/asi/api_generica_spring_boot/domain/entities/task/task.java
+++ src/main/java/com/asi/api_generica_spring_boot/domain/entities/task/task.java
@@ -1,4 +1,33 @@
 package com.asi.api_generica_spring_boot.domain.entities.task;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "\"TAB_TASK\"")
 public class task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="\"ID_TASK\"")
+    private Long idTask;
+
+    @Column(name="\"TITLE_TASK\"")
+    private String titleTask;
+
+    @Column(name="\"DESCRIPTION\"")
+    private String description;
+
+    @Column(name="\"DATE_CREATE\"")
+    private LocalDate dateCreate;
+
+    @Column(name="\"STATUS\"")
+    private String status;
 }
